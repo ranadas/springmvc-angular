fe338ea2da6a63fdd5a538ebacc84895751e8454
Rana Das <rana.pratap.das@gmail.com>
2016-09-29 06:26:53 +0100

    first working commit

---
+++ src/main/java/com/javacodegeeks/examples/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.javacodegeeks.examples.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String homepage(){
+        return "homepage";
+    }
+}
