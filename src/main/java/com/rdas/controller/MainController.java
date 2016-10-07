621ac635a7b2e7e68c103757665fad1b6d752fbe
Rana Das <rana.pratap.das@gmail.com>
2016-10-07 08:12:47 +0100

    new post endpoint - wip

--- src/main/java/com/rdas/controller/MainController.java
+++ src/main/java/com/rdas/controller/MainController.java
@@ -1,7 +1,12 @@
 package com.rdas.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
@@ -9,5 +14,13 @@ public class MainController {
     @RequestMapping("/")
     public String homepage(){
         return "homepage";
+    }
+
+
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.POST, path = "angpost")
+    public ResponseEntity savePage(@RequestBody String formData) throws Exception {
+        System.out.println("-->> formData " + formData);
+        return new ResponseEntity("success", HttpStatus.OK);
     }
 }
